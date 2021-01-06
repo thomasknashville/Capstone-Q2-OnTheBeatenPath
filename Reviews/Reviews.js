64c8cdfcbb0f1c1daa8d54bee0f92f39d85affd9
ronorourke3 <rono.business@gmail.com>
2021-01-06 16:29:06 -0500

    Beginning to set up Reviews page

---
+++ Reviews/Reviews.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+function Reviews() {
+  return (
+    <div>
+      <div className="header">
+        <h1>Leave a review and a picture of your favorite parks!</h1>
+        <h1>signup/signin</h1>
+      </div>
+      <div className="review nav">Link to park pages</div>
+    </div>
+  );
+}
+
+export default Reviews;
